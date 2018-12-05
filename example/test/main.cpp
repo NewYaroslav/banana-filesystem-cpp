ed9bf1337b19c94d9638854c1b326817995517fa
Yaroslav <elektroyar@yandex.ru>
2018-12-05 14:46:55 +0300

    изменено описание

--- example/test/main.cpp
+++ example/test/main.cpp
@@ -5,11 +5,22 @@ using namespace std;
 
 int main()
 {
-        cout << "Hello world!" << endl;
+        cout << bf::get_current_work_dir() << endl;
         std::vector<std::string> list_files;
-        bf::get_list_files("..//..//", list_files, true);
+        bf::get_list_files("..//..//", list_files, true, true);
+
         for(size_t i = 0; i < list_files.size(); ++i) {
                 std::cout << list_files[i] << std::endl;
         }
+
+        string path = "C:/Users\\user/Downloads/VK audio";
+
+        std::vector<std::string> list_dir;
+        bf::parse_path(path, list_dir);
+
+        for( auto & p : list_dir ) cout << "-> " << p << endl;
+
+        bf::create_directory("..//..//test//test2//test3");
+        bf::remove_dir("..\\..\\test", true);
         return 0;
 }
--- include/banana_filesystem.hpp
+++ include/banana_filesystem.hpp
@@ -1,5 +1,5 @@
 /*
-* binary-cpp-api - Binary C++ API client
+* banana-filesystem-cpp - Simple library for working with the file system
 *
 * Copyright (c) 2018 Elektro Yar. Email: git.electroyar@gmail.com
 *
@@ -26,10 +26,7 @@
 //------------------------------------------------------------------------------
 #include <vector>
 #include <string>
-#include <sstream>
 #include <fstream>
-#include <string>
-#include <iostream>
 #include <stdlib.h>
 #include <dirent.h>
 #include <dir.h>
@@ -57,16 +54,17 @@ namespace bf
          * Поиск файлов во вложенных папках при этом не производится
          * Если параметр указан как true, тогда имя файла формируется с учетом path
          * Также функция тогда делает поиск во всех вложенных папках
+         * \param is_folders_only Данная функция будет помещать в спсиок только папки
          * \return количество найденых файлов
          */
 
         unsigned long get_list_files(std::string path,
                                      std::vector<std::string> &output_list,
-                                     bool is_with_directory = false)
+                                     bool is_with_directory = true,
+                                     bool is_folders_only = false)
         {
                 DIR* directory;                                    // Directory object
                 dirent* directory_name;
-                //char str_input_file[FILENAME_MAX] = {0};
                 unsigned long num_files = 0;
 
                 if(path.back() != '/' && path.back() != '\\')
@@ -78,11 +76,16 @@ namespace bf
                                 char * search_char = (char*) memchr(directory_name->d_name, '.', strlen(directory_name->d_name));
                                 std::string str_directory_name = std::string(directory_name->d_name);
                                 if(search_char == NULL) {
+                                        if(is_folders_only && is_with_directory)
+                                                output_list.push_back(path + str_directory_name);
+                                        else if(is_folders_only && !is_with_directory)
+                                                output_list.push_back(str_directory_name);
+
                                         if(is_with_directory)
-                                                num_files += get_list_files(path + str_directory_name, output_list, is_with_directory);
+                                                num_files += get_list_files(path + str_directory_name, output_list, is_with_directory, is_folders_only);
                                         continue;
                                 }
-                                if(str_directory_name == "." || str_directory_name == "..")
+                                if(str_directory_name == "." || str_directory_name == ".." || is_folders_only)
                                         continue;
                                 std::string file_name;
                                 if(is_with_directory)
@@ -94,6 +97,96 @@ namespace bf
                 }
                 closedir(directory);
                 return num_files;
+        }
+//------------------------------------------------------------------------------
+        /** \brief Разобрать путь на составляющие
+         * Данная функция парсит путь, например C:/Users\\user/Downloads разложит на
+         * C: Users user и Downloads
+         * \param path путь, который надо распарсить
+         * \param output_list список полученных элементов
+         */
+        void parse_path(std::string path, std::vector<std::string> &output_list)
+        {
+                if(path.back() != '/' && path.back() != '\\')
+                        path += "/";
+                std::size_t start_pos = 0;
+                while(true) {
+                        std::size_t found_beg = path.find_first_of("/\\", start_pos);
+                        if(found_beg != std::string::npos) {
+                                std::size_t len = found_beg - start_pos;
+                                if(len > 0)
+                                        output_list.push_back(path.substr(start_pos, len));
+                                start_pos = found_beg + 1;
+                        } else break;
+                }
+        }
+//------------------------------------------------------------------------------
+        /** \brief Создать путь
+         * \param path путь, который необходимо создать
+         */
+        void create_directory(std::string path)
+        {
+                std::vector<std::string> dir_list;
+                parse_path(path, dir_list);
+                std::string name;
+                for(int i = 0; i < dir_list.size(); i++) {
+                        name += dir_list[i] + "\\";
+                        if(dir_list[i] == "..")
+                                continue;
+                        std::cout << name << std::endl;
+                        mkdir(name.c_str());
+                }
+        }
+//------------------------------------------------------------------------------
+        /** \brief Получить директорию исполняемого файла
+         * \return строка содержащая директорию
+         */
+        std::string get_current_work_dir()
+        {
+                char current_work_dir[FILENAME_MAX];
+                _getcwd(current_work_dir, sizeof(current_work_dir));
+                return std::string(current_work_dir);
+        }
+//------------------------------------------------------------------------------
+        /** \brief Удалить директорию
+         * Данная функция удаляет директорию со всеми файлами и папками
+         * \param path директория
+         * \param is_delete_root_directory если true, удаляет корневой каталог (который первым указан в path), иначе оставляет паку
+         */
+        void remove_dir(std::string path, bool is_delete_root_directory = false)
+        {
+                DIR* directory;                                    // Directory object
+                dirent* directory_name;
+                if(path.back() != '/' && path.back() != '\\')
+                        path += "/";
+                if(directory = opendir(path.c_str())) {
+                        // открываем директорию
+                        while(directory_name = readdir(directory)) {
+                                unsigned long str_len = strlen(directory_name->d_name);
+                                char * search_char = (char*) memchr(directory_name->d_name, '.', strlen(directory_name->d_name));
+
+                                std::string str_directory_name = std::string(directory_name->d_name);
+                                std::string abs_path = path + str_directory_name;
+
+                                if(search_char == NULL) {
+                                        remove_dir(abs_path);
+                                        rmdir(abs_path.c_str());
+                                        continue;
+                                }
+
+                                if(str_directory_name == "." || str_directory_name == "..")
+                                        continue;
+
+                                FILE *file = NULL;
+                                if(file = fopen(abs_path.c_str(), "r")) {
+                                        fclose(file);
+                                        remove(abs_path.c_str());
+                                }
+                        }
+                }
+                closedir(directory);
+                if(is_delete_root_directory)
+                        rmdir(path.c_str());
         }
 }
 
